60fecd371eaed21008197559e11d3aefb61071a8
unitys27 <nihanbayrakk@gmail.com>
2022-10-11 00:24:04 +0200

    order

---
+++ trying.py
@@ -0,0 +1,16 @@
+#ternary conditionals
+#answer=input("am i sooo cute person and beauuuutiful women and amazingggg?")
+#print("happy to me!") if answer=="absolutely" else 0
+
+average=0
+x=0
+
+while x<= 100:
+    average+=x
+    x+=1
+print(average)
+
+while x<3:
+    print(x)
+    x=x+1
+#şart sağlandığı sürece while döngüsü devam eder. test
